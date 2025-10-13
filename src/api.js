4a69470a652497303f19e0cae56a244811b2d45b
savickuse <savickuse@users.noreply.github.com>
2025-10-13 15:29:22 +0300

    refactor: fix async handling

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 16
 module.exports = API;
+
+
+// API module for BassoonPlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 17
+module.exports = API;
