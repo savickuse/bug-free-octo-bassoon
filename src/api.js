f0641238d47225ad1d716eaaad639ea1d5e3ea21
savickuse <savickuse@users.noreply.github.com>
2026-01-23 00:52:01 +0200

    add: update tests

--- src/api.js
+++ src/api.js
@@ -185,3 +185,30 @@ class API {
 
 // Update 72
 module.exports = API;
+
+
+// API module for BassoonPlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 89
+module.exports = API;
