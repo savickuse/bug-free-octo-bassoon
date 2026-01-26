701c0ca25dbe7d45cdbb329a6ae6ee1de29f98e3
savickuse <savickuse@users.noreply.github.com>
2026-01-26 15:17:36 +0200

    remove: fix data validation

--- src/api.js
+++ src/api.js
@@ -212,3 +212,30 @@ class API {
 
 // Update 89
 module.exports = API;
+
+
+// API module for BassoonPlayer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 91
+module.exports = API;
